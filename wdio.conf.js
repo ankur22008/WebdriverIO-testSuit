e9d360bbaffdfedf158da183992afb249a332f1a
Ankur Jindal <ankur22008@gmail.com>
2019-01-30 15:51:36 +1100

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -205,8 +205,8 @@ exports.config = {
         source: true,       // <boolean> hide source uris
         profile: [],        // <string[]> (name) specify the profile to use
         strict: false,      // <boolean> fail if there are any undefined or pending steps
-        tags: [],           // <string[]> (expression) only execute the features or scenarios with tags matching the expression
-        tagExpression: '@login or @aaa',
+        tags: ['@aaa'],           // <string[]> (expression) only execute the features or scenarios with tags matching the expression
+    //    tagExpression: '@login or @aaa',
         //  tags: process.env.CUCUMBER_TAGS || "",
         timeout: 20000,     // <number> timeout for step definitions
         ignoreUndefinedDefinitions: false, // <boolean> Enable this config to treat undefined definitions as warnings.
